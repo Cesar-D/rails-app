376351e58d5744bbdb6eb4624b76010534bc7e28
Cesar Diaz <cesar.diaz.argomedo@gmail.com>
2025-09-02 00:16:54 +0000

    feat: add controller and view for createArticle

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,4 +3,14 @@ class ArticlesController < ApplicationController
         @articles = Article.all
     end
 
+    def new
+        @article = Article.new
+    end
+
+    def create
+        @article = Article.new(name: params[:article][:name], description: params[:article][:description])
+        @article.save
+        redirect_to articles_path
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  post "/articles", to: "articles#create"
   get "/articles", to: "articles#index"
+  get "/articles/new", to: "articles#new"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")

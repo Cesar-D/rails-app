da8fd645a4bc577c20658c6402fba4a68566cb6c
Cesar Diaz <cesar.diaz.argomedo@gmail.com>
2025-09-15 01:29:26 +0000

    feat: implement user registration with password handling; add users controller, model, and views

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,24 @@
+class UsersController < ApplicationController
+
+    def new
+        
+    end
+
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            # Handle successful signup
+            session[:user_id] = @user.id
+            redirect_to root_path, notice: "Sign up successful!"
+        else
+            # Handle failed signup
+            render :new, status: :unprocessable_entity
+        end
+    end
+
+    private
+
+    def user_params
+        params.require(:session).permit(:email, :password, :password_confirmation)
+    end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_08_25_014421) do
+ActiveRecord::Schema[7.1].define(version: 2025_09_15_005436) do
   create_schema "auth"
   create_schema "extensions"
   create_schema "graphql"
@@ -35,6 +35,14 @@ ActiveRecord::Schema[7.1].define(version: 2025_08_25_014421) do
   create_table "articles", force: :cascade do |t|
     t.string "name"
     t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password_digest"
+    t.boolean "is_confirmed?", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
